fc918ce6848db6d7ddee52c1c690d51f0dc67f88
Mikael Olsson <mikael.olsson@emmio.se>
2019-04-10 14:23:47 +0200

    Added some example data.

--- www/excersises/24/002-cookies.php
+++ www/excersises/24/002-cookies.php
@@ -1,4 +1,31 @@
 <?php // session_start(); ?>
+<pre>
+<?php
+$data = [
+    [
+        'id' => 12,
+        'name' => 'Product 1',
+        'price' => 123
+    ],
+    [
+        'id' => 13,
+        'name' => 'Product 2',
+        'price' => 26
+    ],
+    [
+        'id' => 22,
+        'name' => 'Product 3',
+        'price' => 200
+    ]
+];
+
+/*
+print_r($data);
+print_r(json_encode($data));
+*/
+
+?>
+</pre>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,12 +40,13 @@
     <pre>
     <?php print_r($_COOKIE); ?>
     <?php
-        if (isset($_COOKIE['foo'])) {
-            echo '<h4>Foo</h4>';
-            var_dump($_COOKIE['foo']);
-        }
+    if (isset($_COOKIE['foo'])) {
+        echo '<h4>Foo</h4>';
+        var_dump($_COOKIE['foo']);
+    }
     ?>
     </pre>
+    <textarea id="dataFromDB"><?php echo json_encode($data); ?></textarea>
     <button id="btnSetCookie">Set cookie</button>
     <button id="btnSetCookieArray">Set cookie array</button>
     <button id="btnShowCookies">Show cookies</button>
